a57c706ba1cfdff78bc88e1a98df43bfe4a58fc0
aaryyya <aryampatil2005@gmail.com>
2025-09-20 12:31:52 +0530

    Autowiring

--- src/main/java/org/example/Dev.java
+++ src/main/java/org/example/Dev.java
@@ -1,37 +1,43 @@
 package org.example;
 
 public class Dev {
-    private Laptop laptop;
-    private int age;
+//    private Laptop laptop;
+//    private int age;
 //    public int getAge(){
 //        return age;
 //    }
 //    public void setAge(int age){
 //        this.age=age;
 //    }
-
-    public Laptop getLaptop() {
-        return laptop;
-    }
-    public void setLaptop(Laptop laptop){
-        this.laptop=laptop;
-    }
-
-
+    private Computer comp;
+//    public Laptop getLaptop() {
+//        return laptop;
+//    }
+//    public void setLaptop(Laptop laptop){
+//        this.laptop=laptop;
+//    }
     public Dev(){
-        System.out.println("Dev Constructor");
+    System.out.println("Dev Constructor");
     }
-    public Dev(int age){
-        this.age=age;
-        System.out.println("Dev 1 Constructor and the constructor is parameterised");
+//    public Dev(int age){
+//        this.age=age;
+//        System.out.println("Dev 1 Constructor and the constructor is parameterised");
+//    }
+    public Computer getComp(){
+        return comp;
+    }
+    public void setComp(Computer comp){
+        this.comp=comp;
     }
 
-    public Dev(Laptop laptop){
-        this.laptop=laptop;
-        System.out.println("Dev 1 Constructor and the constructor is parameterised Laptop Constructor");
-    }
+
+//    public Dev(Laptop laptop){
+//        this.laptop=laptop;
+//        System.out.println("Dev 1 Constructor and the constructor is parameterised Laptop Constructor");
+//    }
+
     public void build(){
         System.out.println("Working on this Awesome Project");
-        laptop.compile();
+        comp.compile();
     }
 }
